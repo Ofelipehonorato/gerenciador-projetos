d48d9befc203ee3b49b95f10fde48a2f8b5bb9fb
Felipe Franciscato Honorato <fefranciscato@gmail.com>
2023-01-19 15:34:56 -0300

    Estruturação do Projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,44 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
+// Router - Rotas para o NAV do site
+// Routes - Para Alterar as paginas do projeto
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/NewProject">Novo Projeto</Link>
+        </ul>
+      </div>
+      
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' 
+              element={<Home/>}>
+          </Route>
+          <Route exact path='/company' 
+              element={<Company/>}>
+          </Route>
+          <Route exact path='/contact' 
+              element={<Contact/>}>
+          </Route>
+          <Route exact path='/newproject' 
+              element={<NewProject/>}>
+          </Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
